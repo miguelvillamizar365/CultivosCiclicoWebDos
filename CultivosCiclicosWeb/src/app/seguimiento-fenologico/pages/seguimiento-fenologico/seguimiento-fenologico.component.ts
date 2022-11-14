a7e5a3252bdd3a8de8605447884b5f4687ed2225
miguelvillamizar365 <logimpunk10@hotmail.com>
2022-11-13 20:34:23 -0500

    mvillamizar
    Se realiza check in del componente en vacio de seguimiento fenologico.

---
+++ CultivosCiclicosWeb/src/app/seguimiento-fenologico/pages/seguimiento-fenologico/seguimiento-fenologico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seguimiento-fenologico',  
+  templateUrl: './seguimiento-fenologico.component.html',
+  styleUrls: []
+})
+export class SeguimientoFenologicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
